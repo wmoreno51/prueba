61ca49dd4783f875d69d0ee4dce645fd792c3d9e
Usuario <Usuario@Usuario-PC>
2017-11-08 14:34:59 -0500

    primer comentario

---
+++ src/condicionif/CondicionIf.java
@@ -0,0 +1,18 @@
+package condicionif;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CondicionIf {
+
+    public static void main(String[] args) {
+        
+        //Condicional If-Else        
+        int var = -2;
+        int var2 = 7;
+        if(var > (-3) && var2<3){
+            System.out.println("hola mundo");
+        }
+    }
+}
